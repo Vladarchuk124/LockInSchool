d913a373d07b6b9890aca967bdecd6e2a6e8b2fe
vladarchuk <vladarchukroman@gmail.com>
2025-06-09 19:17:11 +0300

    fix(FAQ): set different font family

--- src/components/FAQ/FAQitem.jsx
+++ src/components/FAQ/FAQitem.jsx
@@ -9,10 +9,10 @@ function FAQitem({ question, answer }) {
         <div style={{ borderBottom: "1px solid #ccc", padding: "30px 0" }}>
             <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", cursor: "pointer" }}
                 onClick={() => setIsOpen(prev => !prev)}>
-                <h3 style={{fontSize: "35px"}}>{question}</h3>
+                <h3 style={{fontSize: "35px", fontFamily: "Arial"}}>{question}</h3>
                 {isOpen ? <Minus size={30} /> : <Plus size={30} />}
             </div>
-            {isOpen && (<p style={{marginTop: "50px", color: "#555", fontSize: "25px"}}>{answer}</p>)}
+            {isOpen && (<p style={{marginTop: "50px", color: "#555", fontSize: "25px", fontFamily: "Arial"}}>{answer}</p>)}
         </div>
     );
 }
