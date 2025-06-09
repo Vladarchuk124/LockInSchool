8c57687329de5004a6a0dd05e707b71c5c6378f7
vladarchuk <vladarchukroman@gmail.com>
2025-06-09 19:27:58 +0300

    chore: responsive design for AskQuestion section

--- src/components/AskQuestion/AskQuestion.jsx
+++ src/components/AskQuestion/AskQuestion.jsx
@@ -1,18 +1,24 @@
+import { useMediaQuery } from 'react-responsive';
 
 function AskQuestion () {
+    const isTabletWidth = useMediaQuery({ maxWidth: 1350});
+    const isMobileWidth = useMediaQuery({ maxWidth: 1240});
+
     const imgStyle = {
+        display: isMobileWidth? "none" : "",
         height: "800px",
-        borderRadius: "14px"
+        borderRadius: "14px",
+        marginLeft: "15px"
     }
 
 
     return (
-        <div style={{display: "flex", justifyContent: "center", gap: "150px", minHeight: "100dvh"}}>
-            <img src="./dog.jpg" alt="dogImage" style={imgStyle}/>
-            <div style={{marginTop: "100px"}}>
+        <div style={{display: "flex", justifyContent: "center", gap: isTabletWidth? "80px" : "150px", minHeight: "100dvh"}}>
+            <img src="LockInSchool/dog.jpg" alt="dogImage" style={imgStyle}/>
+            <div style={{marginTop: "100px", textAlign: isMobileWidth? "center" : ""}}>
                 <h1 style={{color: "#1b3eb5", fontSize: "55px"}}>Залишилися питання?</h1>
                 <p style={{fontSize: "25px"}}>Потрібна допомога з вибором навчання?</p>
-                <h2 style={{fontSize: "35px"}}>Напиши нам у <a style={{color: "black", cursor: "pointer"}} href="https://t.me/arinavenher" target="_blanket">телеграм</a> </h2>
+                <h2 style={{fontSize: "35px", fontFamily: "Montserrat"}}>Напиши нам у <a style={{color: "black", cursor: "pointer", fontFamily: "Montserrat"}} href="https://t.me/arinavenher" target="_blanket">телеграм</a> </h2>
                 <p style={{fontSize: "25px"}}>Ми раді відповісти на усі твої питання!</p>
             </div>
         </div>
