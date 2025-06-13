e79f6ce262edbf7d60a7ce0a60c65de8924158fb
vladarchuk <vladarchukroman@gmail.com>
2025-06-13 15:28:50 +0300

    fix(AskQuestion): source fix

--- src/components/AskQuestion/AskQuestion.jsx
+++ src/components/AskQuestion/AskQuestion.jsx
@@ -1,10 +1,10 @@
 import { useMediaQuery } from 'react-responsive';
 
-function AskQuestion () {
-    const isMobileWidth = useMediaQuery({ maxWidth: 1240});
+function AskQuestion() {
+    const isMobileWidth = useMediaQuery({ maxWidth: 1240 });
 
     const imgStyle = {
-        display: isMobileWidth? "none" : "",
+        display: isMobileWidth ? "none" : "",
         height: "800px",
         borderRadius: "14px",
         marginLeft: "15px"
@@ -12,13 +12,13 @@ function AskQuestion () {
 
 
     return (
-        <div style={{display: "flex", justifyContent: "center", gap: "80px", minHeight: "100dvh"}}>
-            <img src="LockInSchool/questionPageImage.jpg" alt="questionImage" style={imgStyle}/>
-            <div style={{marginTop: "100px", textAlign: isMobileWidth? "center" : ""}}>
-                <h1 style={{color: "#1b3eb5", fontSize: "55px"}}>Залишилися питання?</h1>
-                <p style={{fontSize: "25px"}}>Потрібна допомога з вибором навчання?</p>
-                <h2 style={{fontSize: "35px", fontFamily: "Montserrat"}}>Напиши нам у <a style={{color: "black", cursor: "pointer", fontFamily: "Montserrat"}} href="https://t.me/arinavenher" target="_blanket">телеграм</a> </h2>
-                <p style={{fontSize: "25px"}}>Ми раді відповісти на усі твої питання!</p>
+        <div style={{ display: "flex", justifyContent: "center", gap: "80px", minHeight: "100dvh" }}>
+            <img src="./questionPageImage.jpg" alt="questionImage" style={imgStyle} />
+            <div style={{ marginTop: "100px", textAlign: isMobileWidth ? "center" : "" }}>
+                <h1 style={{ color: "#1b3eb5", fontSize: "55px" }}>Залишилися питання?</h1>
+                <p style={{ fontSize: "25px" }}>Потрібна допомога з вибором навчання?</p>
+                <h2 style={{ fontSize: "35px", fontFamily: "Montserrat" }}>Напиши нам у <a style={{ color: "black", cursor: "pointer", fontFamily: "Montserrat" }} href="https://t.me/arinavenher" target="_blanket">телеграм</a> </h2>
+                <p style={{ fontSize: "25px" }}>Ми раді відповісти на усі твої питання!</p>
             </div>
         </div>
     );
