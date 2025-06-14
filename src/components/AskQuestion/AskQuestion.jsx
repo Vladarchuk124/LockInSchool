5806b60346229eed06c5573d5e766ede232c5019
vladarchuk <vladarchukroman@gmail.com>
2025-06-14 12:33:36 +0300

    fix: font-family fixes

--- src/components/AskQuestion/AskQuestion.jsx
+++ src/components/AskQuestion/AskQuestion.jsx
@@ -15,10 +15,10 @@ function AskQuestion() {
         <div style={{ display: "flex", justifyContent: "center", gap: "80px", minHeight: isMobileWidth ? "60dvh" : "100dvh" }}>
             <img src="questionPageImage.jpg" alt="questionImage" style={imgStyle} />
             <div style={{ marginTop: "100px", textAlign: isMobileWidth ? "center" : "" }}>
-                <h1 style={{ color: "#1b3eb5", fontSize: isMobileWidth ? "35px" : "55px" }}>Залишилися питання?</h1>
-                <p style={{ fontSize: isMobileWidth ? "15px" : "25px" }}>Потрібна допомога з вибором навчання?</p>
-                <h2 style={{ fontSize: isMobileWidth ? "25px" : "35px", fontFamily: "Montserrat" }}>Напиши нам у <a style={{ color: "black", cursor: "pointer", fontFamily: "Montserrat" }} href="https://t.me/arinavenher" target="_blanket">телеграм</a> </h2>
-                <p style={{ fontSize: isMobileWidth ? "18px" : "25px" }}>Ми раді відповісти на усі твої питання!</p>
+                <h1 style={{ color: "#1b3eb5", fontSize: isMobileWidth ? "35px" : "55px", margin: "0" }}>Залишилися питання?</h1>
+                <p style={{ fontSize: isMobileWidth ? "15px" : "25px", margin: "0 0 30px" }}>Потрібна допомога з вибором навчання?</p>
+                <h2 style={{ fontSize: isMobileWidth ? "25px" : "35px", fontFamily: "Montserrat", margin: "0" }}>Напиши нам у <a style={{ color: "black", cursor: "pointer", fontFamily: "Montserrat" }} href="https://t.me/arinavenher" target="_blanket">телеграм</a> </h2>
+                <p style={{ fontSize: isMobileWidth ? "18px" : "25px", margin: "0" }}>Ми раді відповісти на усі твої питання!</p>
             </div>
         </div>
     );
