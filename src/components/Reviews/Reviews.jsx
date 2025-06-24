7c27335d46dc8980d7813d0d2e3fefdd71087938
vladarchuk <vladarchukroman@gmail.com>
2025-06-24 19:34:17 +0300

    fix: add new reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -6,7 +6,7 @@ function Reviews() {
         <div style={{ minHeight: "100dvh" }}>
             <div className={styles.textPart}>
                 <h1>Відгуки наших учнів</h1>
-                <img src="./emoji/emoji4.png" alt="emoji" className={styles.textEmoji} />
+                <img src="LockInSchool/emoji/emoji4.png" alt="emoji" className={styles.textEmoji} />
             </div>
             <div className={styles.upperPart}>
                 <ImageSection
@@ -16,24 +16,18 @@ function Reviews() {
                     reviewStyle="reviewTanya"
                 />
                 <ImageSection
+                    reviewSource="./Reviews/rev3.png"
+                    emojiStyle="emojiHrist"
+                    reviewStyle="reviewHrist"
+                />
+                <ImageSection
                     emojiSource="./emoji/group.png"
                     reviewSource="./Reviews/rev2.png"
                     emojiStyle="emojiKatya"
                     reviewStyle="reviewKatya"
                 />
-                <ImageSection
-                    reviewSource="./Reviews/rev3.png"
-                    emojiStyle="emojiHrist"
-                    reviewStyle="reviewHrist"
-                />
             </div>
             <div className={styles.lowerPart}>
-                <ImageSection
-                    emojiSource="./emoji/rocket.png"
-                    reviewSource="./Reviews/rev4.png"
-                    emojiStyle="emojiLisa"
-                    reviewStyle="reviewLisa"
-                />
                 <ImageSection
                     emojiSource="./emoji/emoji5.png"
                     reviewSource="./Reviews/rev5.png"
@@ -41,13 +35,32 @@ function Reviews() {
                     reviewStyle="reviewNastya"
                 />
                 <ImageSection
-                    emojiSource="./emoji/emoji7.png"
-                    reviewSource="./Reviews/rev6.png"
+                    emojiSource="./emoji/rocket.png"
+                    reviewSource="./Reviews/rev4.png"
+                    emojiStyle="emojiLisa"
+                    reviewStyle="reviewLisa"
+                />
+                <ImageSection
+                    emojiSource="./emoji/emoji6.png"
+                    reviewSource="./Reviews/rev7.png"
+                    emojiStyle="emojiMar"
+                    reviewStyle="reviewMar"
+                />
+            </div>
+            <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+                <ImageSection
+                    emojiSource="./emoji/group.png"
+                    reviewSource="./Reviews/rev8.png"
+                    emojiStyle="emojiElm"
+                    reviewStyle="reviewElm"
+                />
+                <ImageSection
+                    emojiSource="./emoji/americanFlagEmoji.png"
+                    reviewSource="./Reviews/rev9.png"
                     emojiStyle="emojiMira"
                     reviewStyle="reviewMira"
                 />
             </div>
-
         </div>
     );
 }
